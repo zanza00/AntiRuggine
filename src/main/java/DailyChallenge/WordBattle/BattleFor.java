a463d4381467e7d92fcef0eda8300dd0fb5fcdfd
Zanza00 <zanza00@gmail.com>
2015-01-27 22:06:04 +0100

    Extracted the methods for compare two strings and to print the results

--- src/main/java/DailyChallenge/WordBattle/BattleFor.java
+++ src/main/java/DailyChallenge/WordBattle/BattleFor.java
@@ -18,13 +18,49 @@ public class BattleFor {
         do {
             input = sc.nextLine();
             if (input.equalsIgnoreCase("exit"))  break;
-
-
-
+            
             String[] str_array = input.split(" ");
-            String firstString = str_array[0];
-            String secondString = str_array[1];
+            
+            WordBattle wordBattle = new WordBattle(str_array[0], str_array[1]).battle();
+            String firstWord = wordBattle.getFirstString();
+            String secondWord = wordBattle.getSecondString();
 
+            scoringSystem(firstWord, secondWord);
+
+            System.out.println("enter another two words or \"exit\"");
+            } while (!input.equalsIgnoreCase("exit"));
+
+
+
+    }
+
+    private static void scoringSystem(String firstString, String secondString) {
+        if (firstString.length() == secondString.length()) {
+            System.out.println("draw");
+        } else {
+            System.out.println(firstString.length() > secondString.length() ? "first win" : "second win");
+        }
+    }
+
+
+    private static class WordBattle {
+        private String firstString;
+        private String secondString;
+
+        public WordBattle(String firstString, String secondString) {
+            this.firstString = firstString;
+            this.secondString = secondString;
+        }
+
+        public String getFirstString() {
+            return firstString;
+        }
+
+        public String getSecondString() {
+            return secondString;
+        }
+
+        public WordBattle battle() {
             for(int i = 0; i < firstString.length(); i++) {
                 for(int j = 0; j < secondString.length(); j++) {
                     if (firstString.charAt(i) == secondString.charAt(j)) {
@@ -33,21 +69,7 @@ public class BattleFor {
                     }
                 }
             }
-
-//          scoring system
-            if (firstString.length() == secondString.length()) {
-                System.out.println("draw");
-            } else {
-                System.out.println(firstString.length() > secondString.length() ? "first win" : "second win");
-            }
-
-                System.out.println("enter another two words or \"exit\"");
-            } while (!input.equalsIgnoreCase("exit"));
-
-
-
+            return this;
+        }
     }
-
-
-
 }
