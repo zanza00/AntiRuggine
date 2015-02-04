d78a729429e524246ecf080eae9f63453bbf47d7
zanza00 <zanza00@gmail.com>
2015-02-04 10:32:21 +0100

    first stub for working with files

---
+++ src/main/java/files/ReadTxt.java
@@ -0,0 +1,27 @@
+package files;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by @Simone on 04/02/2015.
+ */
+public class ReadTxt {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("testing.txt"))) {
+
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println(sCurrentLine);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
