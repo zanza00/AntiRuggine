41f92053686e74c0870565fcf5652f3810ee8872
Zanza00 <zanza00@gmail.com>
2015-03-18 20:05:37 +0100

    removed double animal in rest request

--- src/main/JS/Flickr/flickr_demo.js
+++ src/main/JS/Flickr/flickr_demo.js
@@ -3,7 +3,7 @@
 $(function () {
     var animalSearch = 'owl';
     var urlForAPI = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6902943f0a9e5a3d4e84475e392ca8e7&format=json&nojsoncallback=1&sort=relevance&text=' + animalSearch;
-    $.getJSON(urlForAPI + animalSearch,
+    $.getJSON(urlForAPI ,
         function (data) {
             var maxRandInt = data.photos.total; //il numero degli elementi massimo
             $.each(data.photos.photo, function (i, item) {
