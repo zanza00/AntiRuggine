52cbefb1bc91c060a42b45d039584b798cf5ec2e
Zanza00 <zanza00@gmail.com>
2015-03-18 21:27:13 +0100

    sometimes the image would't appear, fixed by change the max random by perpage instead of total

--- src/main/JS/Flickr/flickr_demo.js
+++ src/main/JS/Flickr/flickr_demo.js
@@ -5,9 +5,11 @@ $(function () {
     var urlForAPI = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6902943f0a9e5a3d4e84475e392ca8e7&format=json&nojsoncallback=1&sort=relevance&text=' + animalSearch;
     $.getJSON(urlForAPI ,
         function (data) {
-            var maxRandInt = data.photos.total; //il numero degli elementi massimo
+            var maxRandInt = data.photos.perpage; //il numero degli elementi massimo, attualmente perpage=100 (default)
+            var randInt = getRandomInt(0, maxRandInt);
+            console.log(maxRandInt);
+            console.log(randInt);
             $.each(data.photos.photo, function (i, item) {
-                var randInt = getRandomInt(0, maxRandInt);
 
                 if (i === randInt) {
                     var photoDimension = 'm';
