d7d7672e5bcd7fb14cc2135194c61cb3b34a53d5
Zanza00 <zanza00@gmail.com>
2015-03-18 23:33:40 +0100

    Dynamic title based on the animal randomly selected

--- src/main/JS/Flickr/flickr_demo.js
+++ src/main/JS/Flickr/flickr_demo.js
@@ -1,11 +1,10 @@
-
-
 $(function () {
     var animalChoices = ['owl', 'otter', 'alpaca', 'frog', 'cat'];
     var animalSearch = animalChoices[getRandomInt(0, animalChoices.length)];
-    console.log(animalSearch);
+    //cambio il titolo della pagina, mettendo il nome dell'animale con la prima lettera maiuscola
+    document.title = animalSearch[0].toUpperCase() + animalSearch.slice(1) + ' Flikr Demo';
     var urlForAPI = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6902943f0a9e5a3d4e84475e392ca8e7&format=json&nojsoncallback=1&sort=relevance&text=' + animalSearch;
-    $.getJSON(urlForAPI ,
+    $.getJSON(urlForAPI,
         function (data) {
             var maxRandInt = data.photos.perpage; //il numero degli elementi massimo, attualmente perpage=100 (default)
             var randInt = getRandomInt(0, maxRandInt);
@@ -18,7 +17,7 @@ $(function () {
                     var photoURL = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_' + photoDimension + '.jpg';
                     $('#images').text(' ');
                     $("<img>").attr({
-                        src: photoURL, class : '.animal'
+                        src: photoURL, class: '.animal'
                     }).appendTo("#images");
                     return false;
                 }
